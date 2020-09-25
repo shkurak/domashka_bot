7a27d341e883d10673017087691f07d42b7d3d32
shkurak <shkurak@phystech.edu>
2020-09-25 14:44:13 +0000

    initial commit

---
+++ bot.py
@@ -0,0 +1,100 @@
+import os
+import time
+import logging
+import gspread
+from gspread.exceptions import CellNotFound
+from test_system import run_code_on_tests
+
+from aiogram import Bot, Dispatcher, executor, types
+from telegram_token import API_TOKEN
+
+
+class Table:
+    def __init__(self, filename='domashka_credits.json'):
+        gc = gspread.service_account(filename='domashka_credits.json')
+        self.sheet = gc.open("Domashka_results").sheet1
+        
+    def find_user(self, username: str):
+        try:
+            return table.sheet.find(username, in_column=1).row
+        except CellNotFound:
+            return -1
+        
+    def put_mark(self, task:str, user_row: str, mark: int):
+        try:
+            col = self.sheet.find(task, in_row=1).col
+#             row = self.sheet.find(user, in_column=1).row
+            return self.sheet.update_cell(user_row, col, mark)['updatedCells'] == 1
+        except Exception:
+            pass
+        return False
+    
+    def sort_by(self, col_name="сума балів", range='A2:ZZ50'):
+        col = self.sheet.find(col_name).col
+        self.sheet.sort((col, 'des'), range=range)
+
+table = Table()
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.answer("Hi!\nI'm Python Domashka Bot!\nSend me your .py file. Results will apear here https://docs.google.com/spreadsheets/d/1RPwfEnb5kpVz6zh5AQamM-Dj3XNfAcTywkY8JU4Gmrc/edit?usp=sharing")
+
+@dp.message_handler(commands=['table', 'results'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.answer("Таблицю результатів можна знайти тут https://docs.google.com/spreadsheets/d/1RPwfEnb5kpVz6zh5AQamM-Dj3XNfAcTywkY8JU4Gmrc/edit?usp=sharing")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Пришли свій розв'язок в файлі з такою ж назвою як і задача та розширенням .py")
+
+@dp.message_handler(content_types=types.ContentTypes.DOCUMENT)
+async def document_recive(message: types.Message):
+    username = message["from"]["username"]
+    
+    user_row = -1 if username is None else table.find_user(username) 
+    
+    if user_row == -1:
+        await message.answer("вашого логіну немає в таблиці, напишіть викладачу аби він вас додав в список учнів")
+        return
+    taskname, extention = message.document.file_name.split(".")
+    if extention != "py":
+        await message.answer("файл повинен мати розширення .py")
+        return
+    if not os.path.exists("tests/" + taskname):
+        await message.answer("немає такої задачі " + taskname)
+        return
+    await message.answer("отримав файл, перевіряю")
+    await types.ChatActions.typing()
+    file_id = message.document.file_id
+    file = await bot.get_file(file_id)
+    where2save = f"solutions/{username}/{taskname}/"
+    os.makedirs(where2save, exist_ok=True)
+    file_location = where2save + "_".join(map(str, list(time.localtime()))) + ".py"
+    await bot.download_file(file.file_path, file_location)
+    tests_pass, tests_num, error_message = run_code_on_tests(file_location, taskname)
+    
+    await message.answer(f"твій розв'язок пройшов {tests_pass} тестів з {tests_num}")
+    if tests_pass != tests_num:
+        await message.answer(f"твоя програма видала таку помилку\n{error_message}")
+    mark = int(tests_pass/tests_num * 10)
+    await message.answer(f"ти отримав {mark} балів")
+
+    table.put_mark(taskname, user_row, mark)
+    table.sort_by()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ test_system.py
@@ -0,0 +1,56 @@
+import os
+import time
+import resource
+import subprocess
+from glob import glob
+
+# Maximal time for execution of subprocesses (in secs).
+TIME_LIMIT = 1 # 1 second
+# Maximal virtual memory for subprocesses (in bytes).
+MAX_VIRTUAL_MEMORY = 64 * 1024 * 1024 # 64 MB
+
+def limit_virtual_memory():
+    # The tuple below is of the form (soft limit, hard limit). Limit only
+    # the soft part so that the limit can be increased later (setting also
+    # the hard limit would prevent that).
+    # When the limit cannot be changed, setrlimit() raises ValueError.
+    resource.setrlimit(resource.RLIMIT_AS, (MAX_VIRTUAL_MEMORY, resource.RLIM_INFINITY))
+
+def launch_process(code_file, test_file):
+    with open(test_file, "r") as input_file:
+        process = subprocess.Popen(['python3', code_file], 
+            stdin=input_file,
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE, 
+            preexec_fn=limit_virtual_memory)
+    return process
+
+
+def run_code_on_test(code_file, test_file):
+    process = launch_process(code_file, test_file)
+    stdout, stderr = '', 'Something went wrong. Contact administrator and report about that.'
+    try:
+        process.wait()
+        stdout, stderr = process.communicate()
+    except subprocess.TimeoutExpired:
+        process.kill()
+        return '', 'Time Limit Exception'
+    return stdout.decode('ascii'), stderr.decode('ascii')
+
+def run_code_on_tests(code_file, task):
+    tests = sorted(glob("tests/" + task + "/input/*.txt"))
+    for i, test_file in enumerate(tests):
+        stdout, stderr = run_code_on_test(code_file, test_file)
+        if stderr != '':
+            return i, len(tests), stderr
+        with open(test_file.replace("input", "output"), "r") as reference_file:
+            reference = reference_file.read()
+        if reference != stdout:
+            return i, len(tests), 'Wrong answer!'
+    
+    return len(tests), len(tests), ''
+
+
+if __name__== '__main__':
+    result = run_code_on_tests('tests/hello_world/hello_world.py', "hello_world")
+    print(result)
